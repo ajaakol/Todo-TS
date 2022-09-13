5620a114ca245d448d0f98b870d4488e7973351d
Aleksanteri Jaakola <91946999+ajaakol@users.noreply.github.com>
2022-09-13 19:15:32 +0300

    More stufferino

--- App.tsx
+++ App.tsx
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+interface IToDo {
+  text: string;
+  completed: boolean;
+}
 
 export default function App() {
+  const [value, setValue] = useState<string>("");
+  const [toDoList, setToDos] = useState<IToDo[]>([]);
+  const [error, showError] = useState<Boolean>(false);
+
+  const handleSubmit = (): void => {
+    if (value.trim())
+      setToDos([...toDoList, { text: value, completed: false }]);
+    else showError(true);
+    setValue("");
+  };
+
+  const toggleComplete = (index: number): void => {
+    const newToDoList = [...toDoList];
+    newToDoList[index].completed = !newToDoList[index].completed;
+    setToDos(newToDoList);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Todo List</Text>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          placeholder="Enter your todo task..."
+          value={value}
+          onChangeText={e => {
+            setValue(e);
+            showError(false);
+          }}
+          style={styles.inputBox}
+        />
+        <Button title="Add Task" onPress={handleSubmit} />
+      </View>
+    
+     
+      {/* {toDoList.length === 0 && <Text>No to do task available</Text>} */}
+      {toDoList.map((toDo: IToDo, index: number) => (
+        <View style={styles.listItem} key={`${index}_${toDo.text}`}>
+          <Text
+            style={[
+              styles.task,
+              { textDecorationLine: toDo.completed ? "line-through" : "none" }
+            ]}
+          >
+            {toDo.text}
+          </Text>
+          <Button
+            title={toDo.completed ? "Completed" : "Complete"}
+            onPress={() => toggleComplete(index)}
+            color={toDo.completed ? 'red' : 'blue'}
+          />
+         
+        </View>
+      ))}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 35,
+    alignItems: "center"
+  },
+  inputWrapper: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20
+  },
+  inputBox: {
+    width: 200,
+    borderColor: "purple",
+    borderRadius: 8,
+    borderWidth: 2,
+    paddingLeft: 8
+  },
+  title: {
+    fontSize: 40,
+    marginBottom: 40,
+    fontWeight: "bold",
+    textDecorationLine: "underline"
+  },
+
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 10
+  },
+  addButton: {
+    alignItems: "flex-end"
+  },
+  task: {
+    width: 200
   },
 });
